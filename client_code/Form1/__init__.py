4c051d755eb23522b7e0fca338b49a0d74a7dd6b
akilanakilan014@gmail.com <akilanakilan014@gmail.com>
2023-11-15 18:46:34 +0000

    Created Form1 and LICENSE.txt; Edited settings

---
+++ client_code/Form1/__init__.py
@@ -0,0 +1,23 @@
+from ._anvil_designer import Form1Template
+from anvil import *
+
+class Form1(Form1Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+def check_color_blindness():
+    # Prompt the user to identify colors from a test image or provide input
+    # You can use color plates like Ishihara plates for color vision tests
+    user_input = input("Please identify the numbers you see in the image: ")
+
+    # Compare the user's input with the expected results for different types of color blindness
+    if user_input == "12":
+        print("You do not have color vision blindness.")
+    elif user_input == "6":
+        print("You have red-green color blindness (protanopia or deuteranopia).")
+    elif user_input == "8":
+        print("You have blue-yellow color blindness (tritanopia).")
+    else:
+        print("Sorry, I couldn't determine your type of color blindness.")
